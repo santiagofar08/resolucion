86a99c9c4a61b90713bff047c1b71fbbfcef61d5
Santiago <164422919+santiagofar08@users.noreply.github.com>
2024-12-12 20:05:53 -0300

    Update config.php

--- libreria/config.php
+++ libreria/config.php
@@ -1,11 +1,4 @@
 <?php
-//define ('DB_HOST','localhost');
-define ('DB_HOST','localhost');
-define ('DB_USER','root');
-define ('DB_PASS','');
-define ('DB_NAME','bib-t1');
-
-
 // Nombre de la session (puede dejar este mismo)
 $usuarios_sesion="pwd";
 class Conexion extends mysqli{
